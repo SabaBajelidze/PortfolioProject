9da9f5c7c1e06e1c15efb32765f66aefe023fc55
Saba Bajelidze <sababajelidze03@gmail.com>
2022-01-29 01:46:45 +0400

    Add files via upload

--- PortfolioProjectSQL.sql
+++ PortfolioProjectSQL.sql
@@ -1,43 +1,62 @@
---we are now going to see our table.
+-- lets name this table smokers
+select * from Smokers
+-- lets remove useless columns
+alter table smokers drop column topictypeid, topicid, measureid, stratificationid1, stratificationid2, stratificationid3, stratificationid4
+--lets see what we got
 select * from smokers
---firstly, lets delte columns we dont need
-alter table smokers drop column 
-DataSource, Data_value_footnote_symbol,
-Data_value_footnote, data_value_std_err,
-Low_confidence_limit, high_confidence_limit,
-sample_size, topictypeid, topicid, measureid, 
-stratificationid1, stratificationid2, stratificationid3, stratificationid4
---i know that some of this information may be useful, however we dont need them in this scenario.
---now, lets see what we got.
+-- we still have some unwanted columns, lets fix it
+alter table smokers drop column datasource, data_value_footnote_symbol, data_value_footnote, data_value_std_err, low_confidence_limit, high_confidence_limit
 select * from smokers
--- we have 15 columns with 38050 rows. problem is, this table contains data from all different questions from survey. lets see all different questions which were asked
-select distinct topicdesc from smokers
---there are 3 distinct topics, lets see each one and explore questions which were asked.
-select distinct measuredesc from smokers where topicdesc = 'Cigarette Consumption (Adults)'
--- we can see 2 distinct questions asked on this topic, lets do this on all topics
-select distinct measuredesc from smokers where topicdesc = 'Cigarette Use (Adults)'
---there are 4 distinct questions about this topic
-select distinct measuredesc from smokers where topicdesc = 'Cessation (Adults)'
---this much information in one table can cause difficulties in understanding the data. so, lets make new tables using each topics.
-select * into CigaretteConsumption from smokers where topicdesc = 'Cigarette Consumption (Adults)'
-select * from CigaretteConsumption
---we have made new table using old one. now lets delete rows in which response is null
-delete from CigaretteConsumption
-where response is null
--- 765 rows affected
-select * from cigaretteconsumption
--- this leaves us with one question, which asks smokers how many cigarettes they use per day.
---lets see distinct answers
-select distinct response from CigaretteConsumption
--- this table is now usable in power BI
--- lets make similar tables using cigarette use and cessation
-select * into CigaretteUse from smokers where topicdesc = 'Cigarette Use (Adults)'
-select * from Cigaretteuse
--- now lets delete rows where response is null and do same for cessation
-delete from CigaretteUse where response is null
---18000 rows affected
-select * into Cessation from smokers where topicdesc = 'Cessation (Adults)'
-select * from Cessation
---now, we have 4 tables, from which we created 3. we will be using this 3 tables in power bi to make reports.
+--much better. however, we are not done yet.
+-- lets delte rows(surveys) where sample size is not provided
+delete from Smokers where Sample_Size is null
+--as we can see, we deleted 2009 rows.
+select * from Smokers
+--we dont need column topictype, as every row has same 
+alter table smokers drop column topictype 
+select * from Smokers
+select * from smokers where TopicDesc = 'Cessation (Adults)'
+--as this table do not provide good form of information, we must delete useless data. so, lets delete rows where measuredesc is Quit Attempt in Past Year Among Every Day Cigarette Smokers
+-- as this data dont provide accurate answers.
+delete from smokers where MeasureDesc = 'Quit Attempt in Past Year Among Every Day Cigarette Smokers'
+-- we deleted 1621 more rows.
+-----------------------------------------------------------------------------------------------------------
+select * from Smokers
+select * from smokers where TopicDesc = 'Cigarette Use (Adults)'
+select distinct measuredesc from Smokers where TopicDesc = 'Cigarette Use (Adults)'
+select * from smokers where TopicDesc = 'Cessation (Adults)'
+select distinct measuredesc from Smokers where TopicDesc = 'Cessation (Adults)'
+select * from smokers where TopicDesc = 'Cigarette Consumption (Adults)'
+select distinct measuredesc from Smokers where TopicDesc = 'Cigarette Consumption (Adults)'
+--it is useless to have this much information in one table, so i am going to split this table in more useful and little tables.
+-- lets make new table based on measuredesc - current smoking
+select * into CurrentSmokers from Smokers where MeasureDesc = 'Current Smoking'
+--13842 rows affected, lets see our new table.
+select * from CurrentSmokers
+-- this table is almost usable fro power BI.
+select * from smokers where MeasureDesc = 'smoking status'
+--as we can see, when measuredesc is smoking status and answer is current, it is same as Currentsmokers, so , we will add this info to currentsmokers
+insert into Currentsmokers select * from Smokers where MeasureDesc = 'smoking status' and Response = 'Current'
+--2388 rows affected.
+select * from CurrentSmokers
+--as this data is into currentsmokers, we can remove more columns to make it easy to understand and work on.
+alter table currentsmokers drop column topicdesc, measuredesc, response, data_value_unit, data_value_type
+select * from CurrentSmokers
+--good currentsmokers is done and ready for excel. lets make more tables like this
+select * from smokers where measuredesc = 'Smoking Frequency'
+select * into SmokingFrequency from smokers where measuredesc = 'Smoking Frequency'
+-- we have made another table named smokingfrequency which has 4774 rows.
+select * into NeverSmokers from smokers where MeasureDesc = 'Smoking Status' and Response = 'Never'
+--new table named neversmokers which has 2388 rows
+select * into FormerSmokers from smokers where MeasureDesc = 'Smoking Status' and Response = 'Former'
+--new table named formersmokers which has 2388 rows
+-- lets delete rows which has cessation as topicdesc as its not as important to us 
+delete from smokers where TopicDesc = 'Cessation (Adults)'
+select * from FormerSmokers
+-- lets make last table based on Daily Cigarette Consumption Among Every Day Smokers - Average
+select * into AveragePerDay from smokers where MeasureDesc = 'Daily Cigarette Consumption Among Every Day Smokers - Average'
+--this table has 765 rows.
+--now with new tables ready, we can go to excel and power BI and start making reports
+--we went from having 1 useless table full of very different information to having 5 tables with useable information.
 
 
